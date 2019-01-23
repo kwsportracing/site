3b49ff38fa48c56fc17b886e73e6ca1da004d624
Charlie Drage <charlie@charliedrage.com>
2019-01-23 10:36:30 -0500

    Rotate background

--- js/background.js
+++ js/background.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
   var bgArray = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
-  var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
   var path = '../img/background/';
-  var imageUrl = path + bg;
-  $('#hero').css('background-image', 'url(' + imageUrl +')');
+
+
+  secs = 4;
+  bgArray.forEach(function(img){
+      // caches images, avoiding white flash between background replacements
+      new Image().src = path + img; 
+  });
+
+  function backgroundSequence() {
+    window.clearTimeout();
+    var k = 0;
+    for (i = 0; i < bgArray.length; i++) {
+      setTimeout(function(){ 
+        $('#hero').css('background-image', 'url(' + path + bgArray[k] +')');
+      if ((k + 1) === bgArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+      }, (secs * 1000) * i)	
+    }
+  }
+  backgroundSequence();
 });
+
+
