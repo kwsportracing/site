c4b0d80782c971688c13a546cefccf965b2f4576
Sebastian Hothaza <sebastianhothaza@gmail.com>
2019-01-22 21:18:42 -0500

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var bgArray = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg','8.jpg'];
+  var bgArray = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
   var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
   var path = '../img/background/';
   var imageUrl = path + bg;
